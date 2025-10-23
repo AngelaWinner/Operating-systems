3adb65f536ca1f92f9075b3d846083883c3a83e1
Angela Nekrasova <anjberry28@gmail.com>
2025-10-23 16:17:53 +0300

    Это сравнительная работа 1) по двум стандартам - C++98 и C++20. 2) Здесь выполнена работа на CMake, причем один CMakeList.txt файл создает сразу два .exe файла . project (''Task3CMake'') - в скобках название проекта, его нужно если что поменять

---
+++ Task3-vs-Task3CMake/Task3.cpp
@@ -0,0 +1,197 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <random>
+
+int* array;
+int arraySize;
+int markerThreadsCount;
+
+CRITICAL_SECTION cs;
+HANDLE startEvent;
+HANDLE* cantContinueEvents;
+HANDLE* continueEvents;
+bool* closeThreadFlags;
+
+struct markerThreadParams {
+    int index;
+    markerThreadParams(int indexX) : index(indexX) {};
+};
+
+DWORD WINAPI marker(LPVOID param) {
+    markerThreadParams* params = (markerThreadParams*)param;
+    int threadNumber = params->index;
+    int threadIndex = threadNumber - 1;
+    int five = 5;
+    WaitForSingleObject(startEvent, INFINITE);
+    srand(threadNumber);
+
+    int markedElementCount = 0;
+    std::vector<int> markedIndices;
+
+    while (true) {
+        int randomNumber = rand() % arraySize;
+
+        EnterCriticalSection(&cs);
+        if (array[randomNumber] == 0) {
+            Sleep(five);
+            array[randomNumber] = threadNumber;
+            markedElementCount++;
+            markedIndices.push_back(randomNumber);
+            Sleep(five);
+            LeaveCriticalSection(&cs);
+            continue;
+        }
+        if (array[randomNumber] != 0) {
+            std::cout << "Thread with threadIndex " << threadNumber
+                << ", marked " << markedElementCount
+                << " elements, thread cant mark index " << randomNumber << std::endl;
+            LeaveCriticalSection(&cs);
+
+            SetEvent(cantContinueEvents[threadIndex]);
+            WaitForSingleObject(continueEvents[threadIndex], INFINITE);
+
+            if (closeThreadFlags[threadIndex]) {
+                EnterCriticalSection(&cs);
+                for (int index : markedIndices) {
+                    array[index] = 0;
+                }
+                LeaveCriticalSection(&cs);
+                delete params;
+                return 0;
+            }
+        }
+    }
+    delete params;
+    return 0;
+}
+
+int main() {
+    InitializeCriticalSection(&cs);
+
+    std::cout << "Enter size of array : ";
+    try {
+        if (!(std::cin >> arraySize)) {
+            throw std::runtime_error("Invalid input: not a number");
+        }
+        if (arraySize <= 0) {
+            std::cout << "Uncorrect enter. Try another time :)" << std::endl;
+            return 1;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    array = new int[arraySize]();
+
+    std::cout << "Enter number of marker threads : ";
+    try {
+        if (!(std::cin >> markerThreadsCount)) {
+            throw std::runtime_error("Invalid input: not a number :(");
+        }
+        if (markerThreadsCount <= 0) {
+            std::cout << "Uncorrect enter. Try another time :)" << std::endl;
+            return 1;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    startEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    cantContinueEvents = new HANDLE[markerThreadsCount];
+    continueEvents = new HANDLE[markerThreadsCount];
+    closeThreadFlags = new bool[markerThreadsCount]();
+
+    HANDLE* markerThreads = new HANDLE[markerThreadsCount];
+
+    for (int i = 0; i < markerThreadsCount; i++) {
+        cantContinueEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+        continueEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+        closeThreadFlags[i] = false;
+
+        markerThreadParams* params = new markerThreadParams(i+1);
+        markerThreads[i] = CreateThread(NULL, 0, marker, params, 0, NULL);
+    }
+
+    SetEvent(startEvent);
+
+    int activeThreadsCount = markerThreadsCount;
+    while (activeThreadsCount > 0) {
+        std::vector<HANDLE> activeCantContinueEvents;
+        for (int i = 0; i < markerThreadsCount; i++) {
+            if (!closeThreadFlags[i]) {
+                activeCantContinueEvents.push_back(cantContinueEvents[i]);
+            }
+        }
+
+        WaitForMultipleObjects(activeCantContinueEvents.size(),
+            activeCantContinueEvents.data(),
+            TRUE, INFINITE);
+
+        EnterCriticalSection(&cs);
+        std::cout << "Array contents : ";
+        for (int i = 0; i < arraySize; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+        LeaveCriticalSection(&cs);
+        int threadNumberToClose;
+        do {
+            std::cout << "Enter thread index to close (1-" << markerThreadsCount << ") : ";
+            std::cin >> threadNumberToClose;
+        } while (threadNumberToClose < 1 || threadNumberToClose > markerThreadsCount);
+        int threadIndexToClose = threadNumberToClose-1;
+        if (!closeThreadFlags[threadIndexToClose]) {
+
+            closeThreadFlags[threadIndexToClose] = true;
+            SetEvent(continueEvents[threadIndexToClose]);
+            WaitForSingleObject(markerThreads[threadIndexToClose], INFINITE);
+            
+            activeThreadsCount--;
+
+            EnterCriticalSection(&cs);
+            std::cout << "Array after thread " << threadNumberToClose << " closing : ";
+            for (int i = 0; i < arraySize; i++) {
+                std::cout << array[i] << " ";
+            }
+            std::cout << std::endl;
+            LeaveCriticalSection(&cs);
+
+            for (int i = 0; i < markerThreadsCount; i++) {
+                if (!closeThreadFlags[i]) {
+                    ResetEvent(cantContinueEvents[i]);
+                    SetEvent(continueEvents[i]);
+                }
+            }
+        }
+        else {
+            std::cout << "Thread have already been closed!" << std::endl;
+
+            for (int i = 0; i < markerThreadsCount; i++) {
+                if (!closeThreadFlags[i]) {
+                    ResetEvent(cantContinueEvents[i]);
+                    SetEvent(continueEvents[i]);
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < markerThreadsCount; i++) {
+        CloseHandle(markerThreads[i]);
+        CloseHandle(cantContinueEvents[i]);
+        CloseHandle(continueEvents[i]);
+    }
+    CloseHandle(startEvent);
+    DeleteCriticalSection(&cs);
+    delete[] array;
+    delete[] markerThreads;
+    delete[] cantContinueEvents;
+    delete[] continueEvents;
+    delete[] closeThreadFlags;
+    std::cout << "All threads completed. Program finished." << std::endl;
+    return 0;
+}
---
+++ Task3-vs-Task3CMake/Task3CMake.cpp
@@ -0,0 +1,224 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <mutex>
+#include <thread>
+#include <stdexcept>
+#include <condition_variable>
+#include <chrono>
+
+int arraySize;
+std::vector<int> array;
+int markerThreadsCount;
+std::mutex myMutex;
+
+std::condition_variable* startCV;
+bool startFlag = false;
+
+std::vector<std::condition_variable*> threadCVs;
+std::vector<bool> threadReadyFlags;
+std::vector<bool> continueFlags;
+std::vector<bool> closeThreadFlags;
+
+struct markerThreadParams {
+    int index;
+    markerThreadParams(int indexX) : index(indexX) {};
+};
+
+void marker(markerThreadParams* params) {
+    int threadNumber = params->index;
+    int threadIndex = threadNumber - 1;
+
+    {
+        std::unique_lock<std::mutex> lock(myMutex);
+        startCV->wait(lock, [] { return startFlag; });
+    }
+
+    srand(threadNumber);
+
+    int markedElementCount = 0;
+    std::vector<int> markedIndices;
+
+    while (true) {
+        int randomNumber = rand() % arraySize;
+        bool wasMarked = false;
+        {
+            std::lock_guard<std::mutex> lock(myMutex);
+            if (array[randomNumber] == 0) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+                array[randomNumber] = threadNumber;
+                markedElementCount++;
+                markedIndices.push_back(randomNumber);
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(5));
+                continue;
+            }
+        }
+        if (array[randomNumber] != 0) {
+            {
+                std::lock_guard<std::mutex> lock(myMutex);
+                std::cout << "Thread with threadIndex " << threadNumber
+                    << ", marked " << markedElementCount
+                    << " elements, thread cant mark index " << randomNumber << std::endl;
+            }
+
+            {
+                std::unique_lock<std::mutex> lock(myMutex);
+                threadReadyFlags[threadIndex] = true;
+                threadCVs[threadIndex]->notify_one();
+
+                threadCVs[threadIndex]->wait(lock, [&] {
+                    return continueFlags[threadIndex] || closeThreadFlags[threadIndex];
+                    });
+
+                continueFlags[threadIndex] = false;
+            }
+
+            if (closeThreadFlags[threadIndex]) {
+                std::lock_guard<std::mutex> lock(myMutex);
+                for (int index : markedIndices) {
+                    array[index] = 0;
+                }
+                delete params;
+                return;
+            }
+        }
+    }
+}
+
+int main() {
+    startCV = new std::condition_variable();
+
+    std::cout << "Enter size of array : ";
+    try {
+        if (!(std::cin >> arraySize)) {
+            throw std::runtime_error("Invalid input: not a number");
+        }
+        if (arraySize <= 0) {
+            std::cout << "Uncorrect enter. Try another time :)" << std::endl;
+            delete startCV;
+            return 1;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        delete startCV;
+        return 1;
+    }
+    array.resize(arraySize, 0);
+
+    std::cout << "Enter number of marker threads : ";
+    try {
+        if (!(std::cin >> markerThreadsCount)) {
+            throw std::runtime_error("Invalid input: not a number :(");
+        }
+        if (markerThreadsCount <= 0) {
+            std::cout << "Uncorrect enter. Try another time :)" << std::endl;
+            delete startCV;
+            return 1;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        delete startCV;
+        return 1;
+    }
+
+    threadCVs.resize(markerThreadsCount);
+    threadReadyFlags.resize(markerThreadsCount, false);
+    continueFlags.resize(markerThreadsCount, false);
+    closeThreadFlags.resize(markerThreadsCount, false);
+
+    std::vector<std::thread> markerThreads;
+
+    for (int i = 0; i < markerThreadsCount; i++) {
+        threadCVs[i] = new std::condition_variable();
+    }
+
+    for (int i = 0; i < markerThreadsCount; i++) {
+        markerThreadParams* params = new markerThreadParams(i + 1);
+        markerThreads.emplace_back(marker, params);
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(myMutex);
+        startFlag = true;
+    }
+    startCV->notify_all();
+
+    int activeThreadsCount = markerThreadsCount;
+
+    while (activeThreadsCount > 0) {
+        for (int i = 0; i < markerThreadsCount; i++) {
+            if (!closeThreadFlags[i]) {
+                std::unique_lock<std::mutex> lock(myMutex);
+                threadCVs[i]->wait(lock, [&] { return threadReadyFlags[i]; });
+            }
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(myMutex);
+            std::cout << "Array contents : ";
+            for (int i = 0; i < arraySize; i++) {
+                std::cout << array[i] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        int threadNumberToClose, threadIndexToClose;
+
+        do {
+            std::cout << "Enter thread index to close (1-" << markerThreadsCount << ") : ";
+            std::cin >> threadNumberToClose;
+            threadIndexToClose = threadNumberToClose - 1;
+        } while (threadNumberToClose < 1 || threadNumberToClose > markerThreadsCount ||
+            closeThreadFlags[threadIndexToClose]);
+
+        if (!closeThreadFlags[threadIndexToClose]) {
+            closeThreadFlags[threadIndexToClose] = true;
+            threadReadyFlags[threadIndexToClose] = false;
+            continueFlags[threadIndexToClose] = true;
+
+            threadCVs[threadIndexToClose]->notify_one();
+
+            if (markerThreads[threadIndexToClose].joinable()) {
+                markerThreads[threadIndexToClose].join();
+            }
+
+            activeThreadsCount--;
+
+            {
+                std::lock_guard<std::mutex> lock(myMutex);
+                std::cout << "Array after thread " << threadNumberToClose << " closing : ";
+                for (int i = 0; i < arraySize; i++) {
+                    std::cout << array[i] << " ";
+                }
+                std::cout << std::endl;
+            }
+
+            for (int i = 0; i < markerThreadsCount; i++) {
+                if (!closeThreadFlags[i]) {
+                    threadReadyFlags[i] = false;
+                    continueFlags[i] = true;
+                    threadCVs[i]->notify_one();
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < markerThreadsCount; i++) {
+        if (markerThreads[i].joinable()) {
+            markerThreads[i].join();
+        }
+    }
+
+    delete startCV;
+    for (int i = 0; i < markerThreadsCount; i++) {
+        delete threadCVs[i];
+    }
+
+    std::cout << "All threads completed. Program finished." << std::endl;
+    return 0;
+}
