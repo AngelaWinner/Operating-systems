b8c96a11d908bddd154f8032d3177d0960483479
Angela Nekrasova <anjberry28@gmail.com>
2025-11-06 22:59:07 +0300

    Это улучшенная версия первой лабораторной работы. Дополнен класс Employee и добавлены юнит тесты некоторых методов

--- Task1/Headers/Employee.h
+++ Task1/Headers/Employee.h
@@ -1,7 +1,34 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
 struct Employee
 {
-	int num;
-	char name[10];
-	double hours;
+    int num;
+    char name[10];
+    double hours;
+
+    bool operator<(const Employee& other) const {
+        return num < other.num;
+    }
+    bool operator==(const Employee& other) const {
+        return num == other.num &&
+            strcmp(name, other.name) == 0 &&
+            hours == other.hours;
+    }
+    void writeText(std::ostream& out) const {
+        out << num << " " << name << " " << hours << "\n";
+    }
+    void readText(std::istream& in) {
+        in >> num >> name >> hours;
+    }
 };
+std::ostream& operator<<(std::ostream& out, const Employee& emp) {
+    emp.writeText(out);
+    return out;
+}
+std::istream& operator>>(std::istream& in, Employee& emp) {
+    emp.readText(in);
+    return in;
+}
---
+++ Task1/UnitTest/UnitTest.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Headers/Employee.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+        TEST_METHOD(TestComparisonLessOperator)
+        {
+            Employee emp1{ 1, "Ivanov", 8.0 };
+            Employee emp2{ 2, "Petrov", 7.5 };
+
+            Assert::IsTrue(emp1 < emp2);
+            Assert::IsFalse(emp2 < emp1);
+            Assert::IsFalse(emp1 < emp1);
+        }
+
+        TEST_METHOD(TestComparisonEqualOperator)
+        {
+            Employee emp1{ 1, "Ivanov", 8.0 };
+            Employee emp2{ 1, "Ivanov", 8.0 };
+            Employee emp3{ 1, "Petrov", 8.0 };
+            Employee emp4{ 2, "Ivanov", 8.0 };
+            Employee emp5{ 1, "Ivanov", 7.5 };
+
+            Assert::IsTrue(emp1 == emp2);
+            Assert::IsFalse(emp1 == emp3);
+            Assert::IsFalse(emp1 == emp4);
+            Assert::IsFalse(emp1 == emp5);
+        }
+	};
+}
